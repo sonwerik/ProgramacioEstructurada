5ed3425d9208bf477a466d3a18543bdee0325250
sonwerik <sonwerik@gmail.com>
2024-10-09 14:52:53 +0200

    SEMAFOR ARREGLAT!!!!!!

--- ProgramacioEstructurada/src/SequencialsCondicionalsIteratius/Condicionals/pt9Semafor.java
+++ ProgramacioEstructurada/src/SequencialsCondicionalsIteratius/Condicionals/pt9Semafor.java
@@ -5,6 +5,7 @@ public class pt9Semafor {
         Scanner sc = new Scanner(System.in);
 
         String rol = sc.next();
+
         String semaforTranvies = sc.next();
         String semaforVianants = sc.next();
         String semaforCotxes = sc.next();
@@ -13,30 +14,25 @@ public class pt9Semafor {
         boolean passVianants = semaforVianants.equals("VERD");
         boolean passCotxes = semaforCotxes.equals("VERD");
 
-        if (passTranvies && passVianants || passVianants && passCotxes || passTranvies && passCotxes) {
+        boolean pass = false;
+        boolean colisio = false;
 
-            System.out.println("POSSIBLE COLISIO");
-
-        } else {
-
-            boolean pass = false;
-
-            switch (rol) {
-                case "TRANVIA":
-                    pass = passTranvies;
-                    break;
-                case "VIANANT":
-                    pass = passVianants;
-                    break;
-                case "COTXE":
-                    pass = passCotxes;
-                    break;
-                default:
-                    System.out.println("PARAR");
-            }
-
-            if (pass == true) System.out.println("ENDAVANT");
-            else System.out.println("PARAR");
+        switch (rol) {
+            case "TRANVIA":
+                pass = passTranvies;
+                colisio = passTranvies && (passVianants || passCotxes);
+                break;
+            case "VIANANT":
+                pass = passVianants;
+                colisio = passVianants && (passTranvies || passCotxes);
+                break;
+            case "COTXE":
+                pass = passCotxes;
+                colisio = passCotxes && (passTranvies || passVianants);
+                break;
         }
+        if (colisio) System.out.println("POSSIBLE COLISIO");
+        else if (pass) System.out.println("ENDAVANT");
+        else System.out.println("PARAR");
     }
 }
