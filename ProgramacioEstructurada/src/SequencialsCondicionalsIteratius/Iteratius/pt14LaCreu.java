84671f889e1c1a6a984b3ab0a4fb37b235509b80
Èrik Lledó <sonwerik@gmail.com>
2024-10-14 14:31:05 +0200

    miau avançant iteratius, creu feta i narcicista me quiero matar

--- ProgramacioEstructurada/src/SequencialsCondicionalsIteratius/Iteratius/pt14LaCreu.java
+++ ProgramacioEstructurada/src/SequencialsCondicionalsIteratius/Iteratius/pt14LaCreu.java
@@ -12,18 +12,11 @@ public class pt14LaCreu {
         else {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    System.out.print("*");
+                    if (i == j || i + j == n -1) System.out.print("* ");
+                    else System.out.print("  ");
                 }
                 System.out.println();
             }
-            for (int i = 0; i < n; i++) {
-                System.out.print("*");
-                for (int j = 0; j < n; j++) {
-                    System.out.print(" ");
-                }
-                System.out.println("*");
-            }
-        }
+       }
     }
 }
-//coment random
