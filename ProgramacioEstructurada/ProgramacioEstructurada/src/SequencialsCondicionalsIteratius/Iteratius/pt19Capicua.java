d8be87a11eaf162fed821489e5e8578c4d4c34b7
Èrik Lledó <sonwerik@gmail.com>
2024-10-17 13:33:35 +0200

    tipo nomes he canviat de lloc el directori idk

---
+++ ProgramacioEstructurada/ProgramacioEstructurada/src/SequencialsCondicionalsIteratius/Iteratius/pt19Capicua.java
@@ -0,0 +1,16 @@
+package SequencialsCondicionalsIteratius.Iteratius;
+
+import java.util.Scanner;
+
+public class pt19Capicua {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            System.out.println(i);
+        }
+        System.out.println(n);
+    }
+}
