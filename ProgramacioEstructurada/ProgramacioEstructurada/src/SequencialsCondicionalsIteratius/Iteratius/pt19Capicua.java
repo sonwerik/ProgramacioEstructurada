b99e72ebc7b4bd653ca6c8e6ac72cc8040dddf0b
Èrik Lledó <sonwerik@gmail.com>
2024-10-17 14:30:27 +0200

    capicua gonna die

--- ProgramacioEstructurada/ProgramacioEstructurada/src/SequencialsCondicionalsIteratius/Iteratius/pt19Capicua.java
+++ ProgramacioEstructurada/ProgramacioEstructurada/src/SequencialsCondicionalsIteratius/Iteratius/pt19Capicua.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class pt19Capicua {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         int n = sc.nextInt();
+        int length = 0;
+        while (n > 0) n = n / 10; length++;
+        boolean capicua = false;
 
-        for (int i = 1; i < n; i++) {
-            System.out.println(i);
-        }
-        System.out.println(n);
+        int n1 = n / 100;
+        int n2 = (n % 100) / 10;
+        int n3 = n % 10;
+
     }
 }
